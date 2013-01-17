727dc4ae04b7357cfc2aa6302f109ff6bc39dcd1
Thiago Assis <thiagosil@gmail.com>
2013-01-17 13:21:41 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moofle::Application.config.secret_token = 'a12dff9ee1088396b55872c28b662e4ce970b2515389b2a548c76c8ed71b7ae9a5d4e370b20212263ee4d2f09918c31f25288db5aac18443ec0e3076ef741856'
